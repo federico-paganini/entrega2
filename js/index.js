f04f59be2dede85e5e8a46b237f33b7228a6844e
agustinafleitas <agustinaafd@gmail.com>
2023-08-31 22:06:34 -0300

    Usuario

--- js/index.js
+++ js/index.js
@@ -11,15 +11,5 @@ document.addEventListener("DOMContentLoaded", function(){
         localStorage.setItem("catID", 103);
         window.location = "products.html"
     });
-
-    if(!localStorage.getItem("isLoggedIn")){
-        const usuario = document.getElementById("usuario");
-        usuario.style.display = none;
-    }
-    let email = localStorage.getItem("email");
-    let li_nav = document.getElementById("usuario");
-    li_nav.innerHTML=`<span class ="nav-link">${email}</span>`;
-    
-
 });
 
