2386f3761b73cb4d2519b5169671cefeb2170056
Viviana Teng <viviiteng.-@hotmail.com>
2023-09-13 07:41:50 -0300

    pequeños cambios que había hecho anteriormente
    - agregar usuario en product-info.html
    - agregar DOM

--- js/product-info.js
+++ js/product-info.js
@@ -1,29 +1,30 @@
-var valorRecuperado = localStorage.getItem("ValorID");
-console.log(valorRecuperado);
+// Show Comentarios
+document.addEventListener("DOMContentLoaded", function(e) {
 
-// URL del API de comentarios
-const API_COMMENTS_URL = "https://japceibal.github.io/emercado-api/products_comments/";
+  var ProductID = localStorage.getItem("ValorID");
+  console.log(ProductID);
+  
+  // URL del API de comentarios
+  // const PRODUCT_INFO_COMMENTS_URL = "https://japceibal.github.io/emercado-api/products_comments/";
+  
+  // Función para obtener los comentarios de un producto específico
+  function getComments(productId) {
+    const url = `${PRODUCT_INFO_COMMENTS_URL}${productId}.json`;
+  
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Error al cargar los comentarios (${response.status})`);
+        }
+        return response.json(); 
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error al obtener los comentarios:", error);
+      });
+  }
+  getComments(ProductID);
+});
 
-// Función para obtener los comentarios de un producto específico
-function getComments(productId) {
-  const url = `${API_COMMENTS_URL}${productId}.json`;
-
-  // Realizar una solicitud GET a la URL del API de comentarios
-  fetch(url)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`Error al cargar los comentarios (${response.status})`);
-      }
-      return response.json(); // Parsear la respuesta JSON
-    })
-    .then((data) => {
-      // Aquí puedes trabajar con los datos de los comentarios (data)
-      console.log("Comentarios:", data);
-    })
-    .catch((error) => {
-      console.error("Error al obtener los comentarios:", error);
-    });
-}
-
-// Ejemplo de uso: obtener comentarios para un producto con ID 1
-getComments(valorRecuperado);
