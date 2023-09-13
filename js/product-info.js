c7f14b9263baa17a168d5afbfc82401e4d48f68a
agustinafleitas <agustinaafd@gmail.com>
2023-09-13 13:48:28 -0300

    Mostrar los comentarios en la página web

--- js/product-info.js
+++ js/product-info.js
@@ -18,6 +18,23 @@ document.addEventListener("DOMContentLoaded", function(e) {
       })
       .then((data) => {
         console.log(data);
+
+        //Funcion para mostrar los comentarios
+        function CreateDiv(container, info){ //Función con dos argumentos, para crear un nuevo elemento "div" con información. "Container" es el destino donde se inserta el nuevo "div" y "info" son los datos de texto.
+          let div=document.createElement("div"); //Variable para crear elemento "div"
+          div.textContent=info; //Contenido de texto dentro del elemento "div"
+          container.appendChild(div) //el nuevo "div" se agrega como hijo al "container".
+        }
+        const container=document.getElementById("comments-container");
+
+      data.forEach(comment=>{
+        CreateDiv(container, comment.score); //Se crea un nuevo div para mostrar el "score" (estrellas)
+        CreateDiv(container, comment.user); //Se crea un nuevo div para mostrar el nombre de los usuarios
+        CreateDiv(container, comment.dateTime); //Se crea un nuevo div para mostrar la fecha de la creación de los comentarios
+        CreateDiv(container, comment.description); //Se crea un nuevo div para mostrar el comentario
+      })
+
+      
       })
       .catch((error) => {
         console.error("Error al obtener los comentarios:", error);
